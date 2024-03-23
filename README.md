d04434486ba0a136736d05b51c77966e6245959f
annikajc <ajcruz146@gmail.com>
2024-03-23 17:26:34 -0400

    Added front-end toggle switch and commented out JS/event reader

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # FakeReviewSpotter
+
+An AI-powered Chrome extension that detects and filters out fake product reviews.
+
+## Team
+
+Developed by Annika Joy Cruz, Matthew Mapa, Derek Mo
---
+++ frontend/spotReviews.js
@@ -0,0 +1,10 @@
+// NEED TO FIX
+// document.getElementById("switch-on").addEventListener("click", function() {
+//     var switchMessage = document.getElementById("switch-message");
+//     var switchOn = document.getElementById("switch-on");
+//     if (switchOn.checked) {
+//         switchMessage.innerHTML = "Fake Review Spotter is now ON";
+//     } else {
+//         switchMessage.innerHTML = "Fake Review Spotter is now OFF";
+//     }
+// });
