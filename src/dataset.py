72816f1aca12b7314a1191bf2cdf73b1c8cf7a05
mrmapa <mrmapa0625@gmail.com>
2024-04-10 15:29:50 -0400

    added test script for amazon dataset and added yelp dataset and test script

--- src/dataset.py
+++ src/dataset.py
@@ -39,8 +39,7 @@ class DatasetFakeReviews(torch.utils.data.Dataset):
     
 
 class DataModuleFakeReviews(pl.LightningDataModule):
-
-    def __init__(self, data="./data", batch_size=1, tokenizer = None, max_length = None, num_workers=0):
+    def __init__(self, data="./data", batch_size=1, tokenizer = None, max_length = None, dataset = "amazon", num_workers=0):
         super().__init__()
         # later step: NLP Augmentation
         
@@ -56,37 +55,55 @@ class DataModuleFakeReviews(pl.LightningDataModule):
 
         self.tokenizer = tokenizer
         self.max_length = max_length
+
+        self.dataset = dataset
     
     def prepare_data_per_node(self):
         pass
 
     def setup(self, stage):
-        # assign datasets for train, validation and test
-        
-        # load data in from csv
-        reviews_data = pd.read_csv(os.path.join(self.data, "fake_reviews_dataset.csv"))
-        reviews_data['label'] = np.where(reviews_data['label'] == 'CG', 1, 0)
+        if self.dataset == "amazon":
+            # assign datasets for train, validation and test
+            
+            # load data in from csv
+            reviews_data = pd.read_csv(os.path.join(self.data, "fake_reviews_dataset.csv"))
+            reviews_data['label'] = np.where(reviews_data['label'] == 'CG', 1, 0)
 
-        # split individual classes so dataset remains balanced
-        trainval_1, test_1 = train_test_split(reviews_data[['text_', 'label']][reviews_data.label == 1], test_size=0.2, train_size=0.8, random_state=420)
-        trainval_0, test_0 = train_test_split(reviews_data[['text_', 'label']][reviews_data.label == 0], test_size=0.2, train_size=0.8, random_state=420)
+            # split individual classes so dataset remains balanced
+            trainval_1, test_1 = train_test_split(reviews_data[['text_', 'label']][reviews_data.label == 1], test_size=0.2, train_size=0.8, random_state=420)
+            trainval_0, test_0 = train_test_split(reviews_data[['text_', 'label']][reviews_data.label == 0], test_size=0.2, train_size=0.8, random_state=420)
 
-        train_1, val_1 = train_test_split(trainval_1, test_size=0.1, train_size=0.9, random_state=420)
-        train_0, val_0 = train_test_split(trainval_0, test_size=0.1, train_size=0.9, random_state=420)
+            train_1, val_1 = train_test_split(trainval_1, test_size=0.1, train_size=0.9, random_state=420)
+            train_0, val_0 = train_test_split(trainval_0, test_size=0.1, train_size=0.9, random_state=420)
 
-        # concatenate sets together and shuffle 
-        comb_train = pd.concat([train_1, train_0])
-        comb_val = pd.concat([val_1, val_0])
-        comb_test = pd.concat([test_1, test_0])
+            # concatenate sets together and shuffle 
+            comb_train = pd.concat([train_1, train_0])
+            comb_val = pd.concat([val_1, val_0])
+            comb_test = pd.concat([test_1, test_0])
 
-        comb_train = shuffle(comb_train, random_state=420)
-        comb_val = shuffle(comb_val, random_state=420)
-        comb_test = shuffle(comb_test, random_state=420)
+            comb_train = shuffle(comb_train, random_state=420)
+            comb_val = shuffle(comb_val, random_state=420)
+            comb_test = shuffle(comb_test, random_state=420)
 
-        # initialize datasets
-        self.train = DatasetFakeReviews(data=comb_train.reset_index(), tokenizer=self.tokenizer, max_length=self.max_length)
-        self.val = DatasetFakeReviews(data=comb_val.reset_index(), tokenizer=self.tokenizer, max_length=self.max_length)
-        self.test = DatasetFakeReviews(data=comb_test.reset_index(), tokenizer=self.tokenizer, max_length=self.max_length)
+            # initialize datasets
+            self.train = DatasetFakeReviews(data=comb_train.reset_index(), tokenizer=self.tokenizer, max_length=self.max_length)
+            self.val = DatasetFakeReviews(data=comb_val.reset_index(), tokenizer=self.tokenizer, max_length=self.max_length)
+            self.test = DatasetFakeReviews(data=comb_test.reset_index(), tokenizer=self.tokenizer, max_length=self.max_length)
+
+        elif self.dataset == "yelp":
+            # read data from .csv
+            train_data = pd.read_csv(os.path.join(self.data, "yelp-data/train_val.csv"))
+            val_data = pd.read_csv(os.path.join(self.data, "yelp-data/val_val.csv"))
+            test_data = pd.read_csv(os.path.join(self.data, "yelp-data/test.csv"))
+
+            train_data.rename(columns={'text': 'text_'}, inplace=True)
+            val_data.rename(columns={'text': 'text_'}, inplace=True)
+            test_data.rename(columns={'text': 'text_'}, inplace=True)
+
+            # initialize datasets
+            self.train = DatasetFakeReviews(data=train_data.reset_index(), tokenizer=self.tokenizer, max_length=self.max_length)
+            self.val = DatasetFakeReviews(data=val_data.reset_index(), tokenizer=self.tokenizer, max_length=self.max_length)
+            self.test = DatasetFakeReviews(data=test_data.reset_index(), tokenizer=self.tokenizer, max_length=self.max_length)
 
     def train_dataloader(self):
         return torch.utils.data.DataLoader(self.train, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=True,
---
+++ test.py
@@ -0,0 +1,47 @@
+from src.dataset import DataModuleFakeReviews
+from src.model import FakeReviewsLightning
+import torch
+from pytorch_lightning.callbacks import ModelCheckpoint, ModelSummary, EarlyStopping
+from pytorch_lightning import Trainer
+from clearml import Task
+from transformers import AutoTokenizer
+
+def main():
+    # create ClearML task
+    task = Task.init(project_name="Fake Review Detection",
+                     task_name="Test RoBERTa - v1.0")
+
+    # initialize data module and model
+    cuda_available = torch.cuda.is_available()
+    device = torch.device("cuda" if cuda_available else "cpu")
+    print(device)
+    print("   \   ")
+    print("   ^ ^ ")
+    print("  {'.'}")
+    print("? /  | ")
+    print("\|  || ")
+
+    # enable cudnn benchmarking if available
+    torch.backends.cudnn.benchmark = True if torch.backends.cudnn.is_available() and cuda_available else False
+
+    torch.set_float32_matmul_precision('high')
+
+    # initialize tokenizer
+    tokenizer = AutoTokenizer.from_pretrained("roberta-base")
+
+    datamodule = DataModuleFakeReviews(batch_size=2, tokenizer=tokenizer, max_length=256, num_workers=0)
+
+    model = FakeReviewsLightning.load_from_checkpoint("./checkpoints/amazon_weights.ckpt", clearml_logger=task.get_logger(), device=device).to(device)
+
+    trainer = Trainer(accelerator="gpu", max_epochs=300, profiler="simple",
+                          callbacks=[ModelSummary(4)],
+                          strategy="auto", enable_checkpointing=False)
+    # else:
+    #     print("No GPU")
+
+    # fit model
+    trainer.test(model=model, datamodule=datamodule)
+    
+
+if __name__ == '__main__':
+    main()
