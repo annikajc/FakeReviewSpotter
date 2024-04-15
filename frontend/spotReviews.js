b5c1db87ca1efc704c7ae837d67cd3e89dedcf16
annikajc <ajcruz146@gmail.com>
2024-04-15 18:44:18 -0400

    Merge with pull request #2

--- frontend/spotReviews.js
+++ frontend/spotReviews.js
@@ -1,14 +1,3 @@
-// NEED TO FIX
-// document.getElementById("switch-on").addEventListener("click", function() {
-//     var switchMessage = document.getElementById("switch-message");
-//     var switchOn = document.getElementById("switch-on");
-//     if (switchOn.checked) {
-//         switchMessage.innerHTML = "Fake Review Spotter is now ON";
-//     } else {
-//         switchMessage.innerHTML = "Fake Review Spotter is now OFF";
-//     }
-// });
-
 document.getElementById("switch-on").addEventListener("change", function() {
     var switchMessage = document.getElementById("switch-message");
     var switchOn = document.getElementById("switch-on");
